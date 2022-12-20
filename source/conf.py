ceb182c8466dc83cbbd966d030f5348d5788a8ff
Ziyang Hu <hu.ziyang@cantab.net>
2022-12-20 15:48:39 +0800

    version change

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'The CozoScript Manual'
 copyright = '2022, Ziyang Hu'
 author = 'Ziyang Hu'
-release = '0.2.1'
+release = '0.3.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
