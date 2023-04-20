3c33d625d936db093fbbc38e4830eabb8be9404b
Ziyang Hu <hu.ziyang@cantab.net>
2023-04-20 21:01:02 +0800

    0.6 note

--- source/conf.py
+++ source/conf.py
@@ -22,6 +22,23 @@ extensions = [
     'sphinx_copybutton'
 ]
 
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    # "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
 github_username = 'cozodb'
 github_repository = 'cozo'
 
